e945743842019b793d7d28797728eb0663be182a
Leanghour Taing <leang.hour.tang@outlook.com>
2024-04-30 16:46:02 +0700

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -105,10 +105,8 @@ jQuery(document).ready(function($) {
     this_form.find('.sent-message').slideUp();
     this_form.find('.error-message').slideUp();
     this_form.find('.loading').slideDown();
-    var chat_id = '-477676685'
-    var toten = '1597774416:AAGbN4TwPCwS4PSh4QtvzRojIT8i8N5aLl8'
-    // var toten = '1464068819:AAGHc3Yy4r3OGgUqGQemYYDrTBWDO3qUbZw'
-    // var chat_id = '-458916939'
+    var chat_id = '@leanghourtaing'
+    var toten = '6997448829:AAEdGQPDZGlUt0PteDs-6QQ7X5rcNIJ2zy8'
     $.ajax({
       url:'https://api.telegram.org/bot'+toten+'/sendMessage',
       method:'POST',
@@ -138,9 +136,8 @@ jQuery(document).ready(function($) {
 
 });
 function socialClick(message) {
-  //var chat_id = '-458916939'
-  var chat_id = '-487587096'
-  var toten = '1597774416:AAGbN4TwPCwS4PSh4QtvzRojIT8i8N5aLl8'
+  var chat_id = '@leanghourtaing'
+  var toten = '6997448829:AAEdGQPDZGlUt0PteDs-6QQ7X5rcNIJ2zy8'
   let date = new Date(); //actual time in miliseconds
   let string = date.getFullYear()+'-'+date.getMonth()+1+'-'+date.getDate()+' '+date.getHours()+':'+date.getMinutes()
   $.ajax({
